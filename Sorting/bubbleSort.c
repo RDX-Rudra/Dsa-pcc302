bfdc73c38eb683af6725a9422b787e628170216a
RDX-Rudra <dasrudra234@gmail.com>
2024-03-22 18:18:33 +0530

    dsa

--- Sorting/bubbleSort.c
+++ Sorting/bubbleSort.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
 void bubbleSort(int arr[], int n){
-    for(int i=0; i<n-1; i++){
+    int swaped=1;
+    for(int i=0; i<n-1 && swaped; i++){
+        swaped = 0;
         for(int j=0; j<n-i-1; j++){
             if(arr[j]>arr[j+1]){
-                int temp = arr[i];
+                int temp = arr[j];
                 arr[j] = arr[j+1];
-                arr[j] = temp;
+                arr[j+1] = temp;
+                swaped = 1;
             }
         }
     }
@@ -17,9 +20,9 @@ int main(){
     scanf("%d", &num);
     int arr[num];
     for(int j=0; j<num; j++){
-        scanf("%d ", &arr[j]);
+        scanf("%d", &arr[j]);
     }
-    // bubbleSort(arr, num);
+    bubbleSort(arr, num);
     for(int i=0; i<num; i++){
         printf("%d ", arr[i]);
     }
