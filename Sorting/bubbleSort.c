5f1439d3fc95ca00287f82f6c4da73f735ddd00f
RDX-Rudra <dasrudra234@gmail.com>
2024-03-22 17:20:44 +0530

    dsa

--- Sorting/bubbleSort.c
+++ Sorting/bubbleSort.c
@@ -1,10 +1,27 @@
 #include<stdio.h>
 void bubbleSort(int arr[], int n){
-
+    for(int i=0; i<n-1; i++){
+        for(int j=0; j<n-i-1; j++){
+            if(arr[j]>arr[j+1]){
+                int temp = arr[i];
+                arr[j] = arr[j+1];
+                arr[j] = temp;
+            }
+        }
+    }
 }
 
 int main(){
     int num;
     printf("Enter the number of element: ");
     scanf("%d", &num);
+    int arr[num];
+    for(int j=0; j<num; j++){
+        scanf("%d ", &arr[j]);
+    }
+    // bubbleSort(arr, num);
+    for(int i=0; i<num; i++){
+        printf("%d ", arr[i]);
+    }
+    return 0;
 }
