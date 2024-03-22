9d729b77b1ae46d8f1529899d9dc5615341accab
RDX-Rudra <dasrudra234@gmail.com>
2024-03-22 18:39:17 +0530

    dsa

--- Sorting/selectionSort.c
+++ Sorting/selectionSort.c
@@ -1,6 +1,22 @@
 #include<stdio.h>
 
-
+void selectionSort(int arr[], int n){
+    int minIndex;
+    for(int i=0; i<n-1; i++){
+        minIndex = i;
+        for (int j = i+1; j < n; j++)
+        {
+            if(arr[j]< arr[minIndex]){
+                minIndex = j;
+            }
+        }
+        if(minIndex !=i){
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+}
 
 int main(){
     int num;
