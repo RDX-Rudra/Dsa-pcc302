11852d62d9150b6efbb96bdac61f8727f8523731
RDX-Rudra <dasrudra234@gmail.com>
2024-03-25 12:45:48 +0530

    das

--- Sorting/quickSort.c
+++ Sorting/quickSort.c
@@ -1,7 +1,41 @@
 #include<stdio.h>
 
+void swap(int arr[], int a, int b){
+    int temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
 
+}
 
+int partition(int arr[], int low, int high){
+    int pivot = low;
+    int i=low, j=high;
+    while (i<=high || low >= low)
+    {
+        if(arr[i] >arr[pivot] && arr[j]<arr[pivot]){
+            if(i>j){
+                swap(arr, pivot, j);
+            }
+            else
+                swap(arr, i, j);
+        }
+        else{
+            if(arr[i]<arr[pivot])
+                i++;
+            if(arr[j]>arr[pivot])
+                j++;
+        }   
+    }
+    return pivot;
+}
+
+void quickSort(int arr[], int low, int high){
+    if(high>low){
+        int pivot = partition(arr, low, high);
+        quickSort(arr, low, pivot-1);
+        quickSort(arr, pivot+1, high);
+    }
+}
 
 int main(){
     int num;
