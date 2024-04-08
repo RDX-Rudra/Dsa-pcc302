f71fbe080b28062c75231ec1c10e95c2c1bcde56
RDX-Rudra <dasrudra234@gmail.com>
2024-04-08 15:57:26 +0530

    dsa c

--- tempPractice.c
+++ tempPractice.c
@@ -1,12 +1,17 @@
 #include<stdio.h>
+int fib(int n){
+    int fib0 = 1, fib1 = 1, ans=0;
+    for(int i=0; i<n-2; i++){
+        ans = (fib0 + fib1);
+        fib0 = fib1;
+        fib1 = ans;
+    } 
+    return ans;
+}
 int main(){
-    int arr[100];
-    scanf("%d", &arr);
-    int i=0;
-    while (arr[i] != '\n')
-    {
-        printf("%d\n", arr);
-        
-    }
+    int n;
+    scanf("%d", &n);
+    int ans = fib(n);
+    printf("%d\n", ans);
     
 }
